14e7fa7c2429d757091679de8dcc255ed8a260ce
IRENE <irene.garcialop@alumnos.upm.es>
2020-11-14 13:25:20 +0100

    new styles added

--- react_app/src/components/Catalogue.js
+++ react_app/src/components/Catalogue.js
@@ -2,40 +2,38 @@ import React, { Component } from 'react';
 import { Container, Row, Col, Card, Button, CardDeck } from 'react-bootstrap';
 
 class Catalogue extends Component {
-  state = { showAll: false }
-  showMore = () => this.setState({ showAll: true });
-  showLess = () => this.setState({ showAll: false });
-  
-  displayText(text){
+
+  displayText(text) {
     let limit = 200;
-    let showAll = this.state;
     if (text.length <= limit) {
       return text;
     } else {
-      return text.substring(0,limit)+" ...";
+      return text.substring(0, limit) + " ...";
     }
   }
 
   render() {
-    
     return (
       <div className={"catalogue"}>
-          <div className={"catalogue-header"}>
-            <h1 >Phone catalogue</h1>
-            <p>Know more about our phones!</p>
-          </div>
-          <Container>
-            <Row>
-              <CardDeck>
-                {this.props.phones.map((phone, index) => (
-                  <Col xs={8} sm={6} md={4} lg={3} className="phone-col">
-                    <Card >
+        <div className={"catalogue-header"}>
+          <h1>Phone catalogue</h1>
+          <div className={"line-title"}></div>
+          <p>Know more about our phones!</p>
+        </div>
+        <Container>
+          <Row>
+            <CardDeck>
+              {this.props.phones.map((phone, index) => (
+                <Col xs={8} sm={6} md={4} lg={3} className="col-personalized">
+                  <Card >
                     <Row className={"card-img-personalized"} >
-                      <Card.Img src={phone.imageFileName} className={"phone-img"}/>
-                      </Row>
-                      <Row className={"card-body"}>
+                      <Card.Img src={phone.imageFileName} className={"phone-img"} />
+                    </Row>
+                    <Row className={"card-body-personalized"}>
                       <Card.Body>
                         <Card.Title>{phone.name}</Card.Title>
+                        <div className={"line-card"}>
+                        </div>
                         <Card.Subtitle>{phone.manufacturer}</Card.Subtitle>
                         <br></br>
                         <Card.Text>
@@ -43,16 +41,16 @@ class Catalogue extends Component {
                         </Card.Text>
                       </Card.Body>
                       <Card.Footer className={"card-footer-personalized"}>
-                      <Button variant="link">{'>'} More info</Button>
+                        <Button variant="link">{'>'} More info</Button>
                       </Card.Footer>
-                      </Row>
-                    </Card>
-                  </Col>
-                ))}
-              </CardDeck>
-            </Row>
-          </Container>
-        </div>
+                    </Row>
+                  </Card>
+                </Col>
+              ))}
+            </CardDeck>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
